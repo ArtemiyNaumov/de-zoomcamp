69445132d921c6527ebedfa1096921df3b106599
Artemiy Naumov <artemiy.naumov@leroymerlin.ru>
2023-01-19 23:06:51 +0300

    deleted minor scripts from git

--- week_1_basics_n_setup/docker-test/shell_scripts/postgres.sh
+++
@@ -1,9 +0,0 @@
-winpty docker run -it \
-    -e POSTGRES_USER=root \
-    -e POSTGRES_PASSWORD=root \
-    -e POSTGRES_DB=ny_taxi \
-    -v //c/Users/60129676/Desktop/VSCodeProjects/de-zoomcamp/docker-test/ny_taxi_pg_data:/var/lib/postgresql/data \
-    -p 5431:5432 \
-    --network=pg-network \
-    --name pg-database \
-    postgres:13
